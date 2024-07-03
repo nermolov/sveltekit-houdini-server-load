34bf16cfd18f6b5caaba3211993f1f96f61f47b7
taylor <taylor@nermolov.com>
2024-07-03 12:25:38 -0400

    sveltekit + houdini boilerplate

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://houdini-intro.pages.dev/graphql"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
